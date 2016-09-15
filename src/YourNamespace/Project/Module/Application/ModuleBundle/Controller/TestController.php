85a14ffd47571399fff22a9549faac1b7bcd7f3d
Albert Munoz <regniblod@gmail.com>
2016-09-15 23:44:52 +0200

    Added missing use for Method annotation.

--- src/YourNamespace/Project/Module/Application/ModuleBundle/Controller/TestController.php
+++ src/YourNamespace/Project/Module/Application/ModuleBundle/Controller/TestController.php
@@ -3,6 +3,7 @@
 namespace YourNamespace\Project\Module\Application\ModuleBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
